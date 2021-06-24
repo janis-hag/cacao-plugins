cbbcf43e553c6f07f29c111d21276ea7612d288b
kalao <janis.hagelberg@unige.ch>
2021-06-24 12:46:33 +0000

    Added compile_kalao.sh

---
+++ compile_kalao.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Compile script for milk_package
+# Customize / add you own options
+# Use as-is, or make a local custom copy (private, do not commit)
+
+mkdir -p _build
+cd _build
+
+if [ ! -z $1 ]
+then
+    MILK_INSTALL_ROOT=$1
+fi
+
+if [ -z $MILK_INSTALL_ROOT ]
+then
+    MILK_INSTALL_ROOT=/usr/local
+fi
+
+# find python executable
+pythonexec=$(which python)
+if command -v python3 &> /dev/null
+then
+    pythonexec=$(which python3)
+fi
+
+echo "using python at ${pythonexec}"
+
+
+cmake .. -Dbuild_python_module=ON -DPYTHON_EXECUTABLE=${pythonexec} -DCMAKE_INSTALL_PREFIX=$MILK_INSTALL_ROOT
+# cmake .. -DCMAKE_INSTALL_PREFIX=$MILK_INSTALL_ROOT
+
+NCPUS=`fgrep processor /proc/cpuinfo | wc -l`
+
+sudo cmake --build . --target install -- -j $NCPUS
+
+# # MANUAL stuff - just as reminder
+# # Grab version and link to folder - if you switch versions regularly, don't forget to hack your way around:
+# sudo ln -s /usr/local/milk-<version> /usr/local/milk
+
+# # If it's your first compilation EVER
+# # Check you bashrc for
+# export MILK_ROOT=${HOME}/src/milk  # point to source code directory. Edit as needed.
+# export MILK_INSTALLDIR=/usr/local/milk
+# export PATH=${PATH}:${MILK_INSTALLDIR}/bin
+# export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${MILK_INSTALLDIR}/lib/pkgconfig
