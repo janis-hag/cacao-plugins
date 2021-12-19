ad931ca4158ad26fe4e230761253980ef971630a
Janis Hagelberg <janis.hagelberg@unige.ch>
2021-12-19 20:57:41 +0100

    Adding ncCamRead log silencing

--- KalAO_Nuvu/acquire.c
+++ KalAO_Nuvu/acquire.c
@@ -430,6 +430,14 @@ static errno_t compute_function()
 
 	/********** Reduce logging output ****/
 
+	const char*  cam_read_function_name = "ncCamRead";
+	processinfo_WriteMessage(processinfo, "Disabling temp loggging");
+	error = ncCamSilenceFunctionLogging(cam, cam_read_function_name, 1);
+	if (error) {
+		printf("\nThe error %d happened while disbaling camRead logging\n", error);
+		return error;
+	}
+
 	const char* temperature_function_name = "ncCamGetComponentTemp";
 	processinfo_WriteMessage(processinfo, "Disabling temp loggging");
 	error = ncCamSilenceFunctionLogging(cam, temperature_function_name, 1);
